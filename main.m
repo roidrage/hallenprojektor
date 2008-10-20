4b35d2c612146d6085c6cf44ef8a6084e36623a8
Mathias Meyer <meyer@paperplanes.de>
2008-10-20 19:52:47 +0200

    Initial commit

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  hallenprojektor
+//
+//  Created by Pom on 20.10.08.
+//  Copyright (c) 2008 agelion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
